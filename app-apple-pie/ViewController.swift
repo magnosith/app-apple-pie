898c426647f7562964b64e6af3ea09acd62ff5c9
magnosith <magnosith@icloud.com>
2023-06-12 15:25:20 -0700

    att- to finish

--- app-apple-pie/ViewController.swift
+++ app-apple-pie/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var treeImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
